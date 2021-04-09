24cc6fdfed1e6bebb2fd6bdee2ff36610f9f39d1
Matthew Seah <matscottseah@gmail.com>
2021-04-08 21:31:28 -0700

    show alert when stopping timer

--- FlowTime/Views/Timer/StartStopButton.swift
+++ FlowTime/Views/Timer/StartStopButton.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct StartStopButton: View {
     @EnvironmentObject var timerManager: TimerManager
+    @State var showingAlert = false
     
     var body: some View {
-        RoundedButton(action: timerManager.mode == .running ? {self.timerManager.stop()} : {self.timerManager.start()},
+        RoundedButton(action: timerManager.mode == .running
+                              ? {showingAlert.toggle()}
+                              : {self.timerManager.start()},
                       buttonText: timerManager.mode == .running ? "stop" : "start",
                       width: UIScreen.main.bounds.width / 2)
+            .alert(isPresented:$showingAlert) {
+                Alert(
+                    title: Text("Are you sure you want to stop the timer?"),
+                    primaryButton: .destructive(Text("Yes")) {
+                        self.timerManager.stop()
+                    },
+                    secondaryButton: .cancel()
+                )
+            }
     }
 }
 
